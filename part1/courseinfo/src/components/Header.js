e82bcada79b32ec21b1c9d12b70f1701f2c1b8ed
Jose Luis Palacios <hola@jlpalacios.es>
2023-04-10 16:18:49 +0200

    1.1

---
+++ part1/courseinfo/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Header = ({course}) => {
+  return (
+    <h1>course</h1>
+  )
+}
+
---
+++ part1/courseinfo/src/components/Total.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Total = ({exercises}) => {
+  return (
+    <div>Number of exercises {exercises}</div>
+  )
+}
