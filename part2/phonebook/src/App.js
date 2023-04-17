010c93b6a68daa3d9703b83871f25221c6afdb4d
Jose Luis Palacios <hola@jlpalacios.es>
2023-04-17 13:15:54 +0200

    2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import './App.css';
+
+function App() {
+  const [ persons, setPersons ] = useState([{ name: 'Arto Hellas' }])
+  const [ newName, setNewName ] = useState('')
+
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    let newPersons = [...persons]
+    newPersons.push({name: newName})
+    setPersons(newPersons)
+    setNewName('')
+    document.getElementById('inputName').value=''
+  }
+
+  const newPerson = (e) => {
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div className="App">
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={newPerson} id='inputName'/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.length 
+        ? 
+          persons.map(p => <p key={p.name}>{p.name}</p>) 
+        : 
+          <p>No persons added</p>
+      }
+    </div>
+  );
+}
+
+export default App;
