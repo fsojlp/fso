1fe425e179c8ab737b68c47a929e20c911d3311b
Jose Luis Palacios <hola@jlpalacios.es>
2023-04-25 15:18:03 +0200

    3.6

--- part3a/phonebookBackend/index.js
+++ part3a/phonebookBackend/index.js
@@ -56,6 +56,16 @@ app.use(express.json())
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
     const person = {
         name: body.name,
         number: body.number,
