03730502aa660f86689c77be6b683875ada49a9d
Jose Luis Palacios <hola@jlpalacios.es>
2023-05-14 16:08:00 +0200

    4.4

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogList) => {
+    if (blogList.length === 1){
+        return blogList[0].likes
+    } else {
+        let total = 0
+        blogList.map(b => total += b.likes)
+        return total
+    }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
