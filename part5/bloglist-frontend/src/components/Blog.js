d17286476aecf143e79831db7a4a012f0d82e4ea
Jose Luis Palacios <hola@jlpalacios.es>
2023-05-24 16:32:08 +0200

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Blog = ({blog}) => {
+  const [show, setShow] = useState(false)
+
+  const toogleShow = () => {
+    setShow(!show)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   return (
-    <div>
-        {blog.title} {blog.author}
+    <div style={blogStyle}>
+        {blog.title} <button onClick={toogleShow}>{show ? 'hide' : 'show'}</button><br/>
+        {show 
+        ?
+          <>
+          {blog.url}<br/>
+          likes: {blog.likes} <button>like</button><br/>
+          {blog.author}
+          </>
+        :
+        <></>
+        }
+        
     </div>
   )
 }
