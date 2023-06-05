66728a131aad0ce8367bab030071effa43079537
Jose Luis Palacios <hola@jlpalacios.es>
2023-06-05 11:16:49 +0200

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { Blog } from './Blog'
 
@@ -32,4 +32,31 @@ test('<Blog /> verify that the component only renders the blog title and author'
 
   const url= screen.queryByText(/url/i)
   expect(url).toBeNull()
+})
+
+test('<Blog /> show author and like number when click on show', () => {
+  const testBlog = {
+    title: 'Test Blog',
+    author: 'jlpalacios',
+    likes: 0,
+    url:'https://asdf.asdf',
+    user:{ username:'user' }
+  }
+
+  const testFunc = () => {
+    console.log('test')
+  }
+
+  const component = render(
+    <Blog blog={testBlog} erase={testFunc} like={testFunc} username={'testUser'} />
+  )
+
+  const button = component.getByText('show')
+  fireEvent.click(button)
+
+  const likes= screen.queryByText(/likes/i)
+  expect(likes).toHaveTextContent(0)
+
+  const url= screen.queryByText(/url/i)
+  expect(url).toHaveTextContent('https://asdf.asdf')
 })
