dabd9b056e7bbdd19f83ae08a37def620b2f5f88
Jose Luis Palacios <hola@jlpalacios.es>
2023-06-05 11:26:09 +0200

    5.15

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -60,3 +60,33 @@ test('<Blog /> show author and like number when click on show', () => {
   const url= screen.queryByText(/url/i)
   expect(url).toHaveTextContent('https://asdf.asdf')
 })
+
+test('<Blog /> button like calls 2 times the controller on 2 times click', () => {
+  const testBlog = {
+    title: 'Test Blog',
+    author: 'jlpalacios',
+    likes: 0,
+    url:'https://asdf.asdf',
+    user:{ username:'user' }
+  }
+
+  const testFunc = () => {
+    console.log('test')
+  }
+
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <Blog blog={testBlog} erase={testFunc} like={mockHandler} username={'testUser'} />
+  )
+
+  const buttonShow = component.getByText('show')
+  fireEvent.click(buttonShow)
+
+  const buttonLike = component.getByText('like')
+  fireEvent.click(buttonLike)
+  fireEvent.click(buttonLike)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+
+})
